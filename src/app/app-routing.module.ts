e0ee6eb099f5c2cd012f235cc7492c8febd5f7f7
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-06-13 08:30:36 +0200

    T38

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import {PaginaHomeComponent} from './pagina-home/pagina-home.component';
+import {PersonajesComponent} from './personajes/personajes.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import { InformacionComponent } from './informacion/informacion.component';
+
+const routes: Routes = [
+  {
+    path:'home',
+    component:PaginaHomeComponent
+  },
+  {
+    path:'personajes',
+    component:PersonajesComponent
+  },{
+    path:'aboutus',
+    component:AboutUsComponent
+  },{
+    path:'informacion/:id',
+    component:InformacionComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaHomeComponent } from './pagina-home/pagina-home.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaHomeComponent,
+    PersonajesComponent,
+    AboutUsComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
